019094134777a600e40d04ef247b118814527ef3
Dr3ll <p.hundelsh@gmail.com>
2018-08-27 03:54:39 +0200

    searchable pdfs

--- app/render/modules/books/BooksController.js
+++ app/render/modules/books/BooksController.js
@@ -8,8 +8,25 @@ define(
         app.controller('BooksController', ['$scope',
                 function ($scope) {
 
-
                     $scope.init = function() {
+                        $scope.searchWindowOpen = false;
+                        const searchInWebview = require('electron-in-page-search').default;
+                        const webView = document.getElementById('rules');
+                        //searchInWebview.customSearchWindowHtmlPath =
+                        $scope.search = searchInWebview(webView);
+                        $scope.search.openSearchWindow();
+                    };
+
+                    $scope.keypress = function($event) {
+                        if (!$scope.searchWindowOpen) {
+                            if ($event.ctrlKey && $event.key === 'f') {
+                                $scope.openSearch();
+                            }
+                        }
+                    };
+
+                    $scope.openSearch = function() {
+                        $scope.searchWindowOpen = !$scope.searchWindowOpen;
                     };
 
                     $scope.init();
